dd7064673522cf2c482bc8620aba9820de420708
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-24 17:10:26 +0000

    add fields to pokemon model; add many2many mapping

--- src/main/java/com/example/pokemon/models/Pokemon.java
+++ src/main/java/com/example/pokemon/models/Pokemon.java
@@ -1,20 +1,44 @@
 package com.example.pokemon.models;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Positive;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity (name = "Pokemons")
+@Entity(name = "Pokemons")
 public class Pokemon {
 
     @Id
+    @GeneratedValue
     private Long id;
+    @Positive
+    private Long pokemonId;
+    private String pokemonName;
+    private String ability;
+    private Long baseExperience;
+    private Long height;
+    private Long weight;
+    private String typeOne;
+    private String typeTwo;
 
-    public Pokemon() {}
+
+    @ManyToMany(mappedBy = "favouritePokemons")
+    private List<User> users = new ArrayList<>();
+
+
+    public Pokemon() {
+    }
 
     public Pokemon(Long id) {
         this.id = id;
     }
 
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
 }
