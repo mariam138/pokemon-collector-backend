19cce3d0949dd0e3a9c267266aef5e3528992908
Mariam <mariam@Mariams-MacBook-Pro.local>
2026-02-25 12:10:48 +0000

    update local url to deployed frontend

--- src/main/java/com/example/pokemon/config/SecurityConfig.java
+++ src/main/java/com/example/pokemon/config/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of("https://mark-grayson1.github.io/Pokemon-Collector-Front-End/"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
